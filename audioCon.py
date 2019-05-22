f5f37078fb27bded21bcfcb808b28122af011b5a
Sharath Ramkumar <hi@sharath.pro>
2019-05-22 08:04:12 -0400

    add argparser, tqdm settings, organize

--- audioCon.py
+++ audioCon.py
@@ -2,22 +2,31 @@ import sys
 import numpy as np
 from tqdm import tqdm
 from pydub import AudioSegment
+from argparse import ArgumentParser
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Need 1 argument.")
-        sys.exit(0)
-        
-    audio = AudioSegment.from_file(sys.argv[1], format="mp3")
+
+def convert(inputfile, outputfile, period):
+    audio = AudioSegment.from_file(inputfile, format="mp3")
     audio = audio + AudioSegment.silent(duration=150)
-    
-    eightD = AudioSegment.silent(duration=100)
-    pan = np.sin(np.linspace(0, 2*np.pi, 101))
 
-    for i, chunk in tqdm(enumerate(audio[::100])):
+    eightD = AudioSegment.silent(duration=100)
+    pan = 0.9*np.sin(np.linspace(0, 2*np.pi, period))
+    chunks = list(enumerate(audio[::100]))
+
+    for i, chunk in tqdm(chunks, desc='Converting', unit='chunks', total=len(chunks)):
         if len(chunk) < 100:
             continue
-        newChunk = chunk.pan(pan[i%101])
+        newChunk = chunk.pan(pan[i % period])
         eightD = eightD + newChunk
 
-    eightD.export('output.mp3', format="mp3")
+    eightD.export(outputfile, format="mp3")
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Process some integers.')
+    parser.add_argument('-i', type=str, help='input file')
+    parser.add_argument('-o', type=str, default='output.mp3', help='output file (default: output.mp3)')
+    parser.add_argument('-period', type=int, default=200, help='panning period (default: 200)')
+    args = parser.parse_args()
+
+    convert(args.i, args.o, args.period)
